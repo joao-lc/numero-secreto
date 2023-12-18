90fd21d1f5ca8cfbc1369987409b488cdca26c4b
João Augusto <lacerdajoao2002@gmail.com>
2023-12-18 15:25:41 -0400

    comentarios

--- js-curso-2-aula1/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteador = [];
-let numeroLimite = 10;
+let numeroLimite = 10; //quantidade de numero aleatorio
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
